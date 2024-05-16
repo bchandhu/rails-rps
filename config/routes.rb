d1bb6e1e2cccabb466a965102e718569e0eea5b3
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2024-05-16 19:18:51 +0000

    matched target

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get("/rock", {:controller=>"zebra",:action =>"rock"}) 
+  get("/paper", {:controller=>"zebra",:action =>"paper"}) 
+  get("/scissors", {:controller=>"zebra",:action =>"scissors"})
+  get("/", {:controller=>"zebra",:action =>"home"}) 
+ 
 
 end
